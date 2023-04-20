4f482dca3fe261b44863b106efeba1fe0a83d533
VenkyInampudi <128100169+VenkyInampudi@users.noreply.github.com>
2023-04-20 15:03:30 -0700

    Add files via upload

---
+++ src/main/java/com/Doctorsporject/demodocproject/DemodoctorsprojectApplication.java
@@ -0,0 +1,13 @@
+package com.Doctorsporject.demodocproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemodoctorsprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemodoctorsprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Doctorsporject/demodocproject/DemodoctorsprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Doctorsporject.demodocproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemodoctorsprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
